d6aabe6a1ad02f8ebffc09818f8d718d5951fd10
Absolu <absoluj1@southernct.edu>
2024-02-07 17:55:01 -0500

    New file from class

---
+++ src/main/java/GuessingGame.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class GuessingGame {
+    public static void main(String[] args)  {
+        Scanner scnr = new Scanner(System.in);
+        Random generator = new Random();
+        int secret = generator.nextInt();
+        int maxTrials = 4;
+    
+        int guess;
+        System.out.println("Enter a number between 0 and 10");
+        guess = scnr.nextInt();
+
+        int count = 1;
+        while(guess != secret) {
+            count++;
+            System.out.println("Count: " + count);
+            System.out.println("Enter a number between 0 and 10");
+            guess = scnr.nextInt();
+
+            if (guess == secret) {
+                System.out.println("You win!!!!");
+                System.out.println("Number is " + count);
+                break;
+            }
+
+            if (count == maxTrials) {
+                System.out.println("You've run out of tries.");
+                break;
+            }
+        }
+        
+        scnr.close();
+    }
+    
+}
