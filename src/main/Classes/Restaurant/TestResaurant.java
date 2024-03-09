b5b80656087d491d22ff74dabcb85f22d7582f0c
John Absolu <johnson.absolu@hotmail.com>
2024-03-08 23:25:27 -0500

    Restructured Files System

--- src/main/Classes/Restaurant/TestResaurant.java
+++
@@ -1,6 +0,0 @@
-public class TestResaurant {
-    public static void main(String[] args) {
-        Restaurant myRestaurant = new TestResaurant();
-        System.out.println(myRestaurant);
-    }
-}
--- src/main/Loops/ForLoop.java
+++
@@ -1,9 +0,0 @@
-public class ForLoop {
-  public static void main (String[] args) {
-    int sum = 0;
-    for (int i = 0; i < 10; i++) {
-      sum += i;
-    }
-    System.out.print("Sum: " + sum + "\n");
-  }
-}
